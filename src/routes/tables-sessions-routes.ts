23cf61f276b5b216e0aa9167f465ea1a001f2123
MayconSantos04 <gadsence47@gmail.com>
2026-07-10 20:13:12 -0300

    Here we has added the new funtionally of table avilable.

--- src/routes/tables-sessions-routes.ts
+++ src/routes/tables-sessions-routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { TableSessionsController } from "@/controllers/tables-sessions-controller";
+import { TableSessionsController } from "@/controllers/tables-sessions.controller";
 
 const tablesSessionsRoutes = Router();
 const tablesSessionsController = new TableSessionsController();
