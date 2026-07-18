70a4ba6e185fcba5392a1e3a13aa69cc82a4b8d4
MayconSantos04 <gadsence47@gmail.com>
2026-07-17 22:35:52 -0300

    feat: validation added in creating of table.

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -1,5 +1,7 @@
-import { Request, Response, NextFunction } from "express";
+import knex from "knex";
 import { z } from "zod";
+import { AppError } from "@/utils/App-error";
+import { Request, Response, NextFunction } from "express";
 
 class OrdersController {
   async create(request: Request, response: Response, next: NextFunction) {
@@ -14,7 +16,27 @@ class OrdersController {
         request.body,
       );
 
-      return response.status(201).json();
+      const session = await knex<TableSessionsRepository>("tables-sessions")
+        .where({ id: table_session_id })
+        .first();
+
+      if (!session) {
+        throw new AppError("session table not found");
+      }
+
+      if (session.closed_at) {
+        throw new AppError("this table is closed");
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first();
+
+      if (!product) {
+        throw new AppError("product not found");
+      }
+
+      return response.status(201).json(product);
     } catch (error) {
       next(error);
     }
